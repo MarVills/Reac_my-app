6da1469f21b92532030634ccb86c9b96853fde43
MarVills <villaflormarbenc@gmail.com>
2022-07-25 20:54:48 +0800

    simple learnings for today

--- src/App.js
+++ src/App.js
@@ -1,31 +1,17 @@
-import React, { useState } from 'react'
-
+import React from 'react'
+import './App.css'
+import Card from './Card'
 const App = () =>{
 
-  // const name = 'World'
-  const [count, setCount] = useState(0)
-  const [user, setUser] = useState('')
-
-  const increment = () =>{
-    setCount(count + 1) 
-  }
-
-  const handleOnChange = (e) => {
-    // console.log("e: ", e.target.value)
-    setUser(e.target.value)
-  }
-
-  console.log("user: ", user)
 
   return (
-    <React.Fragment>
-      <div>Count: {count}</div>
-      <button id="increment" onClick={increment}>Increment</button>
-      <br />
-      <input name="user" onChange={handleOnChange} value={user}/>
+    <div className='app-main'>
+      <Card name="Marbs" age={23} gender="Male" title="Mine"/>
+      <Card name="Greg" age={27} gender="Male" title="Dev"/>
+      <Card name="Saiben" age={30} gender="Male" title="Frend"/>
+      <Card name="Jonar" age={24} gender="Male" title="Head"/>
 
-    </React.Fragment>
-  
+    </div>
   )
 }
 
---
+++ src/Card.js
@@ -0,0 +1,18 @@
+import React from "react"
+
+const Card = ({name, age, gender, title}) => {
+
+  // const {name, age, gender} = props
+  
+  return (
+    <div className='card-main'>
+      <h1>{title} Card</h1>
+      <div>Name: {name}</div>
+      <div>Age: {age}</div>
+      <div>Gender: {gender}</div>
+    </div>
+  )
+
+}
+
+export default Card
